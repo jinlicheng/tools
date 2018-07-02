1939c921203298cb77acabbc3770d2eea029d6db
kelvin.jin <jinlich@gmail.com>
2018-07-02 17:01:52 +0800

    docker-proxy ok

--- ansible/docker-proxy/README.md
+++ ansible/docker-proxy/README.md
@@ -1,1 +1,12 @@
 # docker proxy
+
+## run
+```
+ansible-playbook -i hosts docker-proxy.yaml --extra-vars "host=192.168.200.180 port=8123"
+
+```
+
+## check
+```
+ansible -i hosts all -m shell -a "docker info"
+```
--- shell/install-shadowsocks-and-polipo.sh
+++ shell/install-shadowsocks-and-polipo.sh
@@ -51,7 +51,7 @@ apt install -y polipo
 cat <<EOF >> /etc/polipo/config
 socksParentProxy = 127.0.0.1:1080
 socksProxyType = socks5
-proxyAddress = 127.0.0.1
+proxyAddress = 192.168.200.180
 proxyPort = 8123
 EOF
 
