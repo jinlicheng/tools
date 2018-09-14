36ee8e1b90200fd5ee519aed59e650659b654256
jinlicheng <jinlicheng82@163.com>
2018-09-14 09:34:10 +0800

    rancher start modify

---
+++ shell/start-rabbitmq.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -d --name rabbitmq --hostname my-rabbit -p 5671:5671 -p 5672:5672 -p 4369:4369 -p 25672:25672 -p 15671:15671 -p 15672:15672 rabbitmq:management
--- shell/start-rancher.sh
+++ shell/start-rancher.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -d --name rancher-server --restart=unless-stopped -p 8080:80 -p 8443:443 rancher/rancher:v2.0.4 --add-local true
+if [ "$#" -ne "1" ];then
+    echo "USAGE: $0 version"
+    exit 1
+fi
+
+docker run -d --name rancher-server --restart=unless-stopped -p 8080:80 -p 8443:443 rancher/rancher:$1 --add-local true
