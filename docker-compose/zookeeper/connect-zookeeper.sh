802b2cfbfa998e14d5aa856b7726e690f7741d79
kelvin <jinlicheng82@163.com>
2018-09-16 21:23:18 +0800

    add zookeeper

---
+++ docker-compose/zookeeper/connect-zookeeper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+        --link zoo1:zk1 \
+        --link zoo2:zk2 \
+        --link zoo3:zk3 \
+        --net zookeeper_default \
+        zookeeper zkCli.sh -server zk1:2181,zk2:2181,zk3:2181
