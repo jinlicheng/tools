004dbf905b6ef2991c200225cf2012ef33f82cda
kelvin jin <jinlicheng82@163.com>
2020-09-18 09:57:28 +0800

    update gitlab starter

--- shell/start-gitlab-server.sh
+++ shell/start-gitlab-server.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo docker run --detach \
     --hostname 10.20.1.7 \
     --publish 3443:443 --publish 3080:80 --publish 30022:22 \
@@ -7,3 +9,9 @@ sudo docker run --detach \
     --volume /srv/gitlab/logs:/var/log/gitlab \
     --volume /srv/gitlab/data:/var/opt/gitlab \
     gitlab/gitlab-ce:11.0.3-ce.0
+
+echo "modify /srv/gitlab/config/gitlab.rb"
+echo "external_url 'http://192.168.2.102:82'"
+echo "nginx['listen_port'] = 82"
+echo "gitlab_rails['gitlab_shell_ssh_port'] = 2222"
+
